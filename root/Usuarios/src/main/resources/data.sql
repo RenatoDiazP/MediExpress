1d2bfb30f6369fbe0712e3ba5c1390d04e849bca
Renato Díaz <re.diazp@duocuc.cl>
2025-06-22 17:22:44 -0400

    Carga
    Precarga de datos para rol y estado, modifique app properties para que lo inicie al iniciar el programa

---
+++ root/Usuarios/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+-- Estados
+INSERT INTO estado (id_estado, nombre_estado) VALUES (1, 'ACTIVO');
+INSERT INTO estado (id_estado, nombre_estado) VALUES (2, 'INACTIVO');
+
+-- Roles
+INSERT INTO rol (id_rol, nombre_rol) VALUES (1, 'ADMIN');
+INSERT INTO rol (id_rol, nombre_rol) VALUES (2, 'CLIENTE');
+INSERT INTO rol (id_rol, nombre_rol) VALUES (3, 'MODERADOR');
