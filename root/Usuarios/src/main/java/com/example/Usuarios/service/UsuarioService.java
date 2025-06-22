d4590fe06cf3f93c774404456a8a4c741f6aa893
Renato Díaz <re.diazp@duocuc.cl>
2025-06-22 16:27:38 -0400

    arregle algo
    mala mia

--- root/Usuarios/src/main/java/com/example/Usuarios/service/UsuarioService.java
+++ root/Usuarios/src/main/java/com/example/Usuarios/service/UsuarioService.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import com.example.Usuarios.config.Encriptador;
 import com.example.Usuarios.model.Usuario;
 import com.example.Usuarios.repository.UsuarioRepository;
 
