421c6c6075067687dc1ba5926d9623b7d782e344
fanny698 <sa.chamorro@duocuc.cl>
2025-06-23 22:08:32 -0400

    ñe

--- mediexpress/producto-inventario/src/main/java/com/mediexpress/producto_inventario/repository/CategoriaRepository.java
+++ mediexpress/producto-inventario/src/main/java/com/mediexpress/producto_inventario/repository/CategoriaRepository.java
@@ -1,9 +1,11 @@
 package com.mediexpress.producto_inventario.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.mediexpress.producto_inventario.model.Categoria;
 
+@Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
 
 
--- mediexpress/producto-inventario/src/main/java/com/mediexpress/producto_inventario/repository/FilaRepository.java
+++ mediexpress/producto-inventario/src/main/java/com/mediexpress/producto_inventario/repository/FilaRepository.java
@@ -1,9 +1,11 @@
 package com.mediexpress.producto_inventario.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.mediexpress.producto_inventario.model.Fila;
 
+@Repository
 public interface FilaRepository extends JpaRepository<Fila, Long> {
     
 
